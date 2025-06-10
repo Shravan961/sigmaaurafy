64c800e723d6ad368afee544625be733c53dd663
Shravan961 <22svidhya@heckgrammar.co.uk>
2025-06-10 19:00:21 +0100

    Fix empty NutritionCard component

---
+++ src/components/nutrition/NutritionCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface NutritionCardProps {
+  // Add props as needed
+}
+
+const NutritionCard: React.FC<NutritionCardProps> = () => {
+  return (
+    <div className="nutrition-card">
+      {/* Component content will be added later */}
+    </div>
+  );
+};
+
+export default NutritionCard;
